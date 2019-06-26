75a0d3d73dd5bf689cb18816267079b93d54ea60
Bosun Egberinde <bosunski@gmail.com>
2019-06-26 19:22:18 +0100

    Update callable test
    Removed test for callability and changed to test if the value is an instance of `Closure`

--- source/Renderer.php
+++ source/Renderer.php
@@ -648,7 +648,7 @@ class Renderer
                 $key = $flippedAttributes[$properKey];
             }
 
-            if (is_callable($value)) {
+            if ($value instanceof Closure) {
                 $value = $value();
             }
 
